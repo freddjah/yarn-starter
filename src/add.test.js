b26db366b774aaa959677baec9c9cf2bb080b51c
Fredrik Eklööf <fredrik.ekloof@gmail.com>
2019-10-26 12:38:29 +0200

    fix failing test (git push hooks works wohoo)

--- src/add.test.js
+++ src/add.test.js
@@ -4,6 +4,6 @@ test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test("failing test", () => {
-  expect(sum(1, 2)).toBe(2);
+test("add 1 + 1 to equal 2", () => {
+  expect(sum(1, 1)).toBe(2);
 });
